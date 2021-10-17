17921da1df85768d7d1685b1980f112505467102
Masataka Yakura <masataka.yakura@gmail.com>
2021-10-18 00:44:15 +0900

    feat(popup): add functions to check if a line starts with markdown link or URL

--- scabra_popup.js
+++ scabra_popup.js
@@ -3,6 +3,14 @@ const textarea = document.querySelector(`textarea`);
 const button = document.querySelector(`button`);
 const output = document.querySelector(`output`);
 
+function startsWithMarkdownLink(string) {
+	return /\[(?:.+)\]\((?<url>https?\:\/\/.+)\)/.test(string);
+}
+
+function startsWithUrl(string) {
+	return /(?<url>https?\:\/\/.+)/.test(string);
+}
+
 form.addEventListener(`submit`, (event) => {
 	event.preventDefault();
 	const content = textarea.value.trim();
